96990472994142e8573e0c55630c4c89fe14a568
Siddharth Hota <siddharthhota26@gmail.com>
2023-06-18 12:58:57 +0530

    initial

---
+++ src/components/Form.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+
+const Form = () => {
+    let [g, setG] = useState("");
+    function gender(e) {
+        if (e.target.checked) {
+            setG(e.target.value);
+        }
+    }
+
+    return (
+        <div>
+            <h2>Select your gender:</h2>
+            <input type="radio" value={"male"} onClick={gender}></input><label>Male</label>
+            <input type="radio" value={"female"}></input><label>Female</label>
+            {
+                g === "male" ?
+                    <div>
+                        <h2>Select your shirt size:</h2>
+                        <select>
+                            <option>Select size</option>
+                            <option>Small</option>
+                            <option>Medium</option>
+                            <option>Large</option>
+                        </select>
+                    </div>:
+                    <div>
+                    <h2>Select your dress size:</h2>
+                    <select>
+                        <option>Select size</option>
+                        <option>2</option>
+                        <option>4</option>
+                        <option>6</option>
+                    </select>
+                </div>
+
+            }
+        </div>
+    )
+}
+
+export default Form;
